cf26bc83e091b0243c5cf125237a7011c7a899d5
paulao4455 <168849918+paulao4455@users.noreply.github.com>
2024-05-03 14:01:21 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function marcaPonto() {
+    if (xBolinha > 590) {
+        meusPontos += 1;
+        ponto.play();
+    }
+    if (xBolinha < 10) {
+        pontosDoOponente += 1;
+        ponto.play();
+    }
+}
+
+
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
+}
